5d2bfdc7c7d489353aa08ff4034c71a0b6b2dc6e
andy <andy.schulz@gmail.com>
2019-09-30 16:25:02 +0200

    Added trash-cli to list of packages in setup script

--- linux_initial_setup/ubuntu_debian.sh
+++ linux_initial_setup/ubuntu_debian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install vim vim-common vim-gnome vim-gui-common vim-runtime curl tree
+sudo apt-get install vim vim-common vim-gnome vim-gui-common vim-runtime curl tree trash-cli
 
 
