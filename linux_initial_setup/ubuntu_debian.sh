475d6bb05c4982e6317bf875e35cbf42c123a907
andy <andy_schulz_develop@yahoo.com>
2020-12-09 15:50:04 +0100

    Added build and compile tools to Ubuntu script

--- linux_initial_setup/ubuntu_debian.sh
+++ linux_initial_setup/ubuntu_debian.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install vim vim-common vim-gnome vim-gui-common vim-runtime curl tree trash-cli
-
+sudo apt-get install vim vim-common vim-gui-common vim-runtime curl tree trash-cli build-essential dkms linux-headers-generic python3 git gcc make perl g++;
 
