efea93fc62a7d676200a33b6026d9e39d9b3aee4
andy-schulz-develop <55987119+andy-schulz-develop@users.noreply.github.com>
2021-01-13 18:06:29 +0100

    Delete temp directory

--- temp/Analysis/Actions/Moving_Averages/MovingAverage.py
+++
@@ -1,9 +0,0 @@
-
-import decimal;
-
-
-class MovingAverage(object):
-
-    def __init__(self, precision="0.00000001"):
-        self._precision = decimal.Decimal(precision);
-        self._last_timestamp = 0;
