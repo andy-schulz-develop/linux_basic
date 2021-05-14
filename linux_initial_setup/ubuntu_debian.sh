0c0a45f14726e2d0b5ea2ce1bbab4a1786cf4fc4
andy <andy_schulz_develop@yahoo.com>
2021-05-15 00:04:27 +0200

    Added net-tools package to make netstat command available

--- linux_initial_setup/ubuntu_debian.sh
+++ linux_initial_setup/ubuntu_debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install vim vim-common vim-gui-common vim-runtime curl tree trash-cli build-essential dkms linux-headers-generic python3 git gcc make cmake perl g++ mlocate;
+sudo apt-get install vim vim-common vim-gui-common vim-runtime curl tree trash-cli build-essential dkms linux-headers-generic python3 git gcc make cmake perl g++ mlocate net-tools;
 
