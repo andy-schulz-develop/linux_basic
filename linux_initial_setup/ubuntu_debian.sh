3634ababad5e3643ccedf91d25e9635e4156f08b
andy-schulz-develop <55987119+andy-schulz-develop@users.noreply.github.com>
2024-11-21 21:22:11 +0100

    Update ubuntu_debian.sh
    Package mlocate got replaced plocate

--- linux_initial_setup/ubuntu_debian.sh
+++ linux_initial_setup/ubuntu_debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install vim vim-common vim-gui-common vim-runtime curl tree trash-cli build-essential dkms linux-headers-generic python3 git gcc make cmake perl g++ mlocate net-tools;
+sudo apt-get install vim vim-common vim-gui-common vim-runtime curl tree trash-cli build-essential dkms linux-headers-generic python3 git gcc make cmake perl g++ plocate net-tools;
 
